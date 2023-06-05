8062e20bb6e66c1d8540620886561aa85bb97f14
sithydev :v <109386708+sithydev@users.noreply.github.com>
2023-06-05 03:30:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById('update-form');
-  const textarea = document.getElementById('update-text');
-  const updateList = document.getElementById('update-list');
+// Fetch server status and player count from API
+fetch('https://api.mcsrvstat.us/2/5.135.54.213:25607') // Replace with your server IP and port
+  .then(response => response.json())
+  .then(data => {
+    const statusElement = document.getElementById("status-text");
 
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const updateText = textarea.value;
-    if (updateText) {
-      const updateItem = document.createElement('li');
-      updateItem.textContent = updateText;
-      updateList.appendChild(updateItem);
-      textarea.value = '';
+    // Check if the server is online
+    if (data.online) {
+      const playerCount = data.players.online;
+      statusElement.textContent = `Players online: ${playerCount}/${data.players.max}`;
+      statusElement.classList.add("operational");
+    } else if (data.offline) {
+      statusElement.textContent = "OFFLINE";
+      statusElement.classList.add("offline");
+    } else if (data.motd.clean === "WHITELISTED") {
+      statusElement.textContent = "UNDER MAINTENANCE";
+      statusElement.classList.add("maintenance");
     }
+  })
+  .catch(error => {
+    const statusElement = document.getElementById("status-text");
+    statusElement.textContent = "Error fetching server status";
+    statusElement.classList.add("offline");
+    console.error(error);
   });
-});
