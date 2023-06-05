36e7a14364c0207ad67f5d7d1b666fb46fd3ebbf
sithydev :v <109386708+sithydev@users.noreply.github.com>
2023-06-05 03:34:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-// Fetch server status and player count from API
-fetch('https://api.mcsrvstat.us/2/5.135.54.213:25607') // Replace with your server IP and port
-  .then(response => response.json())
-  .then(data => {
-    const statusElement = document.getElementById("status-text");
+document.addEventListener('DOMContentLoaded', function() {
+  const statusElement = document.getElementById('status');
 
-    // Check if the server is online
-    if (data.online) {
-      const playerCount = data.players.online;
-      statusElement.textContent = `Players online: ${playerCount}/${data.players.max}`;
-      statusElement.classList.add("operational");
-    } else if (data.offline) {
-      statusElement.textContent = "OFFLINE";
-      statusElement.classList.add("offline");
-    } else if (data.motd.clean === "WHITELISTED") {
-      statusElement.textContent = "UNDER MAINTENANCE";
-      statusElement.classList.add("maintenance");
-    }
-  })
-  .catch(error => {
-    const statusElement = document.getElementById("status-text");
-    statusElement.textContent = "Error fetching server status";
-    statusElement.classList.add("offline");
-    console.error(error);
-  });
+  const serverIP = '5.135.54.213';
+  const serverPort = 25607;
+
+  function checkServerStatus() {
+    const url = `https://api.mcsrvstat.us/2/5.135.54.213:25607`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        if (data.online) {
+          statusElement.textContent = `Server is online - ${data.players.online} players online`;
+        } else {
+          statusElement.textContent = 'Server is offline';
+        }
+      })
+      .catch(error => {
+        statusElement.textContent = 'Failed to fetch server status';
+        console.error(error);
+      });
+  }
+
+  checkServerStatus();
+});
